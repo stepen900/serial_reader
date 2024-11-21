e303db445e554987984d27f3d2e14e664039cb2e
Nikita <stepen900@ya.ru>
2024-11-21 09:31:31 +0300

    ___

--- window/win_main.py
+++ window/win_main.py
@@ -3,6 +3,7 @@ from PyQt5 import QtCore, QtGui,QtWidgets
 from PyQt5.QtCore  import *
 from PyQt5.QtGui  import *
 from untitled import *
+import serial
 
 
 
@@ -12,10 +13,30 @@ class MyWin(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.setWindowTitle('COM Connector')
-        self.ui
+        self.serial_ports()
+        self.ui.pushButton.clicked.connect(self.get_combobox)
 
-    # def mbox(self, body, title = 'Erroe'):
-    #     dialog = QMessage
+    
+    def get_combobox(self):
+
+        port = self.ui.portBox.currentText()
+        speed = self.ui.speedBox_2.currentText()
+        time_out =  self.ui.comboBox_3.currentText()
+        self.ui.consoleBrowser.clear()
+        self.ui.consoleBrowser.setText(f'{port=} {speed= } {time_out=}')
+        print(port, speed, time_out)
+
+    def serial_ports(self):
+        from serial.tools import list_ports
+        f = list_ports.comports()
+
+        for i in f:
+            g = str(i).split(' ')[0]
+            self.ui.portBox.addItem(g)
+    
+        
+
+
 
 
 if __name__ == '__main__':
