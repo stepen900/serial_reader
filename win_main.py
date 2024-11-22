204a76fa1ac29ade7756b681d8c56deed1f49fa0
Nikita <stepen900@ya.ru>
2024-11-22 08:45:46 +0300

    'Classes'

--- win_main.py
+++ win_main.py
@@ -4,15 +4,12 @@ from PyQt5.QtCore  import *
 from PyQt5.QtGui  import *
 from untitled import *
 
-from open_port import open_port
-from port_read import read_in_serial
-from port_write import write_in_serial
+
 from checksum import checksum
 import logging
-from  port_read import *
-from  open_port import *
-from  port_write import *
-import threading
+from serial.tools import list_ports
+import serial
+
 
 logger = logging.getLogger(__name__)
 FORMAT = '%(levelname)s %(asctime)s %(name)s %(message)s'
@@ -24,36 +21,104 @@ logging.basicConfig(level=logging.INFO, filemode='a',\
 
 
 class MyWin(QtWidgets.QMainWindow):
+
+
     def __init__(self, parent = None):
+        self.classport = Port()
+        self.classport.serial_ports()
         QtWidgets.QWidget.__init__(self, parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.setWindowTitle('COM Connector')
-        self.serial_ports()
+        self.add_item()
         self.ui.pushButton.clicked.connect(self.get_combobox)
-
+        
+        
+    def add_item(self):
+        self.ui.portBox.clear()
+        for i in self.classport.ports:
+                self.ui.portBox.addItem(i)
+    
     
     def get_combobox(self):
 
 
         global port, speed, time_out, port_thread
+
         port = self.ui.portBox.currentText()
         speed = self.ui.speedBox_2.currentText()
         time_out =  self.ui.comboBox_3.currentText()
         self.ui.consoleBrowser.clear()
         self.ui.consoleBrowser.setText(f'{port=} {speed= } {time_out=}')
+        
 
-        ser = open_port(port, int(speed), float(time_out))
-
-    def serial_ports(self):
-        from serial.tools import list_ports
-        f = list_ports.comports()
-
-        for i in f:
-            g = str(i).split(' ')[0]
-            self.ui.portBox.addItem(g)
     
         
+
+
+
+
+class Port():
+
+    def __init__(self):
+        self.ser = None
+
+    def serial_ports(self):
+
+
+        '''Возвращает список COM'''
+
+        f = list_ports.comports()
+        current_ports = []
+        for i in f:
+            g = str(i).split(' ')[0]
+            current_ports.append(g)
+        self.ports = current_ports
+
+
+    def open_port(self, port:str, baud:int, time_out:float):
+
+
+        ''' Получает данные из файла конфига 
+        и открывает порт, возвращая объект соединения'''
+
+        try:
+            ser = serial.Serial(port= port, baudrate= baud, timeout= time_out)
+            logger.info(f'Установил подключение к {com= } \
+                        на скорости {baud= } c  {time_out= }')
+            
+        except Exception:
+            logger.critical(f'Не могу подключиться к {port= } на \
+                            скорости {baud= } c  {time_out= }, пытаюсь снова ...')
+        self.ser = ser
+        return ser
+
+    def write_in_serial(self, command:bytes)-> None:
+
+
+        '''Записывает данные в COM'''
+
+
+        self.ser.write(command)
+        logger.info(f'Данные {command=} записаны в порт')
+
+
+    def read_in_serial(self)-> bytes:
+
+
+        '''Читает данные из COM'''
+
+        logger.info('получаем данные из COM')
+        return self.ser.readline()
+
+
+
+
+
+
+
+
+
 
 
 
@@ -70,12 +135,3 @@ def window_com():
 if __name__ == '__main__':
 
     window_com()
-    
-
-
-
-
-    # ser = open_port()
-    # command = checksum('!AA')
-    # write_in_serial(ser, 0x23)
-    # read_in_serial(ser)
